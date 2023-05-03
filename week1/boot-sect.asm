5293c99bb3b3c6b438c686656345e54608064409
John Li <johnjohnli2015@hotmail.com>
2023-05-03 16:14:52 -0700

    boot loader can print hello world

--- week1/boot-sect.asm
+++ week1/boot-sect.asm
@@ -1,5 +1,21 @@
 ;TODO: add your boot sector code here.
+mov cx, 0x7c00
+
+print_string:
+  mov ah, 0eh ; Tells BIOS to print in teletype mode
+  mov si, msg
+  add si, cx
+  inc cx
+  mov al, [si]
+  int 10h
+  test al, al
+  jnz print_string
+  jmp $
+
+msg:
+  db "Hello world!", 0h
 
 ; Bootsector padding
+jmp print_string
 times 510 -( $ - $$ ) db 0 ; $ is the current memory address and $$ is the start of the file
 dw 0xaa55 ; boot sector signature
