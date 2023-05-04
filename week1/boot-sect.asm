e84845e95b821c6cb4aa2381f63200e6cc1f6e99
John Li <johnjohnli2015@hotmail.com>
2023-05-03 19:03:47 -0700

    printhex now works (?)

--- week1/boot-sect.asm
+++ week1/boot-sect.asm
@@ -2,12 +2,19 @@
 [org 0x7c00]
 mov bx, msg
 call print_string
-msg:
-  db "Hello world!", 0
+mov dx, msg
+call print_hex
+
 
 jmp $
 
+msg:
+  db "Hello world!", 0
+hex_msg:
+  db 0xA1, 0xA1 , 0
+
 %include "print_string.asm"
+%include "print_hex.asm"
 ; Bootsector padding
 times 510 -( $ - $$ ) db 0 ; $ is the current memory address and $$ is the start of the file
 dw 0xaa55 ; boot sector signature
--- week1/print_hex.asm
+++ week1/print_hex.asm
@@ -3,6 +3,42 @@ print_hex: ;dx is parameter
   ;TODO: implement a function that takes in value of dx
   ;and modify the string located at HEX_OUT to be the hex
   ;value. Then print out HEX_OUT.
+  mov bx, HEX_OUT
+  mov di, dx
+  mov cl, 4;
+  
+  get_hex:
+    mov ah, 0eh ; Tells BIOS to print in teletype mode
+    ; get first byte
+    mov al, 0xF
+    shl al, cl
+    and al, [di]
+    shr al, cl
+    jmp put_byte
+  cont: 
+    xor cl, 0x4
+    test cl, cl
+    jz get_hex
+
+    mov al, [di]
+    inc di
+    test al, al
+    jnz get_hex
+
+  mov byte [bx], 0
+  mov bx, HEX_OUT
+  call print_string
+
   popa
   ret
+
+put_byte:
+  cmp al, 9
+  jle put_number
+  add al, 7
+put_number:
+  add al, 0x30
+  mov [bx], al
+  inc bx
+  jmp cont
 HEX_OUT: db '0x0000', 0
