debc4202c17bfd57458463231a5043f9a451b88d
John Li <johnjohnli2015@hotmail.com>
2023-05-03 17:10:01 -0700

    print_string now works

--- week1/boot-sect.asm
+++ week1/boot-sect.asm
@@ -1,21 +1,13 @@
-;TODO: add your boot sector code here.
-mov cx, 0x7c00
-
-print_string:
-  mov ah, 0eh ; Tells BIOS to print in teletype mode
-  mov si, msg
-  add si, cx
-  inc cx
-  mov al, [si]
-  int 10h
-  test al, al
-  jnz print_string
-  jmp $
-
+;boot sector code
+[org 0x7c00]
+mov bx, msg
+call print_string
 msg:
-  db "Hello world!", 0h
+  db "Hello world!", 0
 
+jmp $
+
+%include "print_string.asm"
 ; Bootsector padding
-jmp print_string
 times 510 -( $ - $$ ) db 0 ; $ is the current memory address and $$ is the start of the file
 dw 0xaa55 ; boot sector signature
--- week1/print_string.asm
+++ week1/print_string.asm
@@ -4,5 +4,13 @@ print_string:
     ;TODO: using the BIOS teletype routine, print out the string
     ;located at the address stored in bx stopping at the null
     ;terminating byte
+    print_char:
+        mov ah, 0eh ; Tells BIOS to print in teletype mode
+        mov al, [bx]
+        int 10h
+        inc bx
+        test al, al
+        jnz print_char
+
     popa
     ret
